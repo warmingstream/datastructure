e2f1b2606c8448e908bd22ae363022fd8e36bc4d
wangming <wangming3@jd.com>
2018-04-01 14:52:33 +0800

    耗时两天终于明白二叉树最长距离求解方法，还算OK吧，加油！

---
+++ src/main/java/com/baimawanglang/tree/Depth_Distance.java
@@ -0,0 +1,21 @@
+package com.baimawanglang.tree;
+
+public class Depth_Distance {
+    public int depth;
+    public int distance;
+
+    public Depth_Distance() {
+        this.depth = 0;
+        this.distance = 0;
+    }
+
+    public Depth_Distance(int depth, int distance) {
+        this.depth = depth;
+        this.distance = distance;
+    }
+
+    @Override
+    public String toString() {
+        return "Depth_Distance{" + "depth=" + depth + ", distance=" + distance + '}';
+    }
+}
--- src/main/java/com/baimawanglang/tree/TreeTest.java
+++ src/main/java/com/baimawanglang/tree/TreeTest.java
@@ -9,8 +9,19 @@ public class TreeTest {
         TreeNode root = new TreeNode(30,
                                         new TreeNode(25, new TreeNode(20, null, null), new TreeNode(27, null, null)),
                                         new TreeNode(32, null, new TreeNode(36, null, null)));
-        TreeNode publicNode = getPublicNode(root, new TreeNode(36), new TreeNode(36));
-        System.out.println(publicNode);
+        TreeNode root2 = new TreeNode(30,
+                                        new TreeNode(25, new TreeNode(20, null, new TreeNode(22, new TreeNode(21, null, null), new TreeNode(23, null, null))), new TreeNode(27, null, new TreeNode(28, null, new TreeNode(29, null, null)))),
+                                        new TreeNode(32, null, new TreeNode(40, new TreeNode(34, null, new TreeNode(35, null, new TreeNode(37, null, null))), null)));
+        System.out.println(getTreeDepth(root));
+        System.out.println(getTreeDepth(root2));
+        System.out.println("最长路径" + getDistance(root));
+        System.out.println("最长路径" + getDistance(root2));
+
+        Integer max = 0;
+        System.out.println(get(root, 0));;
+        System.out.println(max);
+        System.out.println(get(root2, 0));;
+        System.out.println(max);
     }
 
     //遍历二叉树
@@ -61,7 +72,29 @@ public class TreeTest {
         if (root == null) {return 0;}
         int deptL = getTreeDepth(root.left);
         int deptR = getTreeDepth(root.right);
-        return deptL > deptR ? deptL : deptR;
+        return deptL > deptR ? ++deptL : ++deptR;
+    }
+
+    //求高度和最大距离
+    public static int get(TreeNode node, Integer max) {
+        if (node == null) {return 0;}
+        int leftD = get(node.left, max) + 1;
+        int rightD = get(node.right, max) + 1;
+        int depth = leftD + rightD;
+        if (max < depth) {max = depth;}
+        System.out.println("max:" + max);
+        return leftD > rightD ? leftD : rightD;
+    }
+
+    //求最长距离
+    public static Depth_Distance getDistance(TreeNode root) {
+        if (root == null) {return new Depth_Distance();}
+        Depth_Distance left = getDistance(root.left);
+        Depth_Distance right = getDistance(root.right);
+        Depth_Distance max = new Depth_Distance();
+        max.depth = Math.max(left.depth + 1, right.depth + 1);
+        max.distance = Math.max(Math.max(left.distance, right.distance), left.depth + right.depth + 2);
+        return max;
     }
 
     //求两节点最近公共父节点
