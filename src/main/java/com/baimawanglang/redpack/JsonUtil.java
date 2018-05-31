3aadcc7f042fb96c124d81bdbc0f2331b779278f
wangming <wangming3@jd.com>
2018-05-31 14:33:06 +0800

    抢红包修改

--- src/main/java/com/baimawanglang/redpack/JsonUtil.java
+++ src/main/java/com/baimawanglang/redpack/JsonUtil.java
@@ -1,7 +1,14 @@
 package com.baimawanglang.redpack;
 
 
-public interface JsonUtil {
+public class JsonUtil {
 
 
+    public static Order jsonToObj(String orderJson, Class<Order> orderClass) {
+        return null;
+    }
+
+    public static String toJson(Order order) {
+        return null;
+    }
 }
